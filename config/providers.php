1f15decd08596f0c0f5a18526e158229bf54690e
terabytesoftw <terabytesoftw@gmail.com>
2020-05-30 13:39:11 -0400

    Add ReverseBlockMerge::class in config.

--- config/providers.php
+++ config/providers.php
@@ -6,10 +6,13 @@ use App\Provider\CacheProvider;
 use App\Provider\EventDispatcherProvider;
 use App\Provider\LoggerProvider;
 use App\Provider\WebViewProvider;
+use Yiisoft\Arrays\Modifier\ReverseBlockMerge;
 
 return [
     'yiisoft/cache' => CacheProvider::class,
     'yiisoft/eventdispatcher' => EventDispatcherProvider::class,
     'yiisoft/log' => LoggerProvider::class,
-    'yiisoft/view' => WebViewProvider::class
+    'yiisoft/view' => WebViewProvider::class,
+
+    ReverseBlockMerge::class => new ReverseBlockMerge()
 ];
